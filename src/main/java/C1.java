18b6202880c8566cf41acc151bcf72f85fd87f3d
irega97 <ivanreq97@gmail.com>
2020-04-15 01:16:47 +0200

    Initial commit

---
+++ src/main/java/C1.java
@@ -0,0 +1,6 @@
+public class C1 implements Command{
+    public String execute() {
+        //We return this "C1" string
+        return "C1";
+    }
+}
---
+++ src/main/java/C2.java
@@ -0,0 +1,6 @@
+public class C2 implements Command{
+    public String execute() {
+        //We return this "C1" string
+        return "C2";
+    }
+}
